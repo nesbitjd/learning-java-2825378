9756075737d2cfe11fa829a62efd352507efd7f4
nesbitjd <nesbitjd@gmail.com>
2021-10-19 15:57:42 -0500

    add solution to github

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
+package src;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String question = "What do Klingons drink in the morning?";
+        String choiceOne = "p'tagh";
+        String choiceTwo = "raktajino";
+        String choiceThree = "romulan ale";
+
+        String correctAnswer = choiceTwo;
+
+        System.out.println(question);
+        System.out.println(choiceOne);
+        System.out.println(choiceTwo);
+        System.out.println(choiceThree);
         
+        String answer = input.next();
+
+        if (correctAnswer.equals(answer.toLowerCase())) {
+            System.out.println("Correct! Klingons drink " + correctAnswer);
+        } else {
+            System.out.println("False, klingons drink " + correctAnswer);
+        }
+
+
     }
 }
